4e8a2cddb04a63cb8716ae3408dc5fb71b7b4e11
jtfl2008 <jtfl2008@gmail.com>
2017-09-27 14:17:10 +0800

    配置管理

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'secret': 'vueNodeExpressMongooseLearn', // used when we create and verify JSON Web Tokens
+  'DB_URL': 'mongodb://localhost:27017/db', // 填写本地自己 mongodb 连接地址,xxx为数据表名
+  'port': 3000
+}
