f838a892f39bf363c404dbf34e876ec25927d121
jtfl2008 <jtfl2008@gmail.com>
2017-10-16 11:41:56 +0800

    路由修改

--- server/app.js
+++ server/app.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const config = require('./config/config.js')
 const bodyParser = require('body-parser')
-
 const index = require('./router/index.js')
-const item = require('./router/lists.js')
-
 const port = config.port
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/api', index)
-
 app.listen(port, () => {
   console.log('app listening in ' + port)
 })
