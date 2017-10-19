ba039da319a9a3270e266c634b2e21cfa5e4718e
jtfl2008 <jtfl2008@gmail.com>
2017-10-19 15:03:16 +0800

    添加内容

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,26 +1,84 @@
-<script src="../../.eslintrc.js"></script>
 <template>
   <div class="detail">
 
     <el-card :body-style="{ padding: '0px' }">
+
       <div class="poster">
         <img :src="detail.cover" class="image" width="200">
       </div>
       <div class="detail-desc">
-        <span class="title">{{detail.title}}</span>
-        <div class="bottom clearfix">
-          <time class="time">{{ detail.currentDate }}</time>
+        <h2 class="movie-title">{{detail.title}}</h2>
+        <div class="item directors">
+          <div class="item-label">
+            导演：
+          </div>
+          <ul class="item-content">
+            <li>
+              {{ detail.directors }}
+            </li>
+          </ul>
         </div>
-        <div class="directors">
-          {{ detail.directors }}
+        <div class="item rate">
+          <div class="item-label">
+            评分：
+          </div>
+          <ul class="item-content">
+            <li>
+              {{ detail.rate }} 分
+            </li>
+          </ul>
         </div>
-        <div class="actors">
-          <ul>
+        <div class="item actors">
+          <div class="item-label">
+            主演：
+          </div>
+          <ul class="item-content">
             <li v-for="item in detail.actors">
               {{ item }}
             </li>
           </ul>
         </div>
+        <div class="item types">
+          <div class="item-label">
+            类型：
+          </div>
+          <ul class="item-content">
+            <li v-for="item in detail.types">
+              {{ item }}
+            </li>
+          </ul>
+        </div>
+        <div class="item region">
+          <div class="item-label">
+            制片国家/地区：
+          </div>
+          <ul class="item-content">
+            <li >
+              {{ detail.region }}
+            </li>
+          </ul>
+        </div>
+        <div class="item release_year">
+          <div class="item-label">
+            上映日期：
+          </div>
+          <ul class="item-content">
+            <li >
+              {{ detail.release_year }}
+            </li>
+          </ul>
+        </div>
+        <div class="item duration">
+          <div class="item-label">
+            片长：
+          </div>
+          <ul class="item-content">
+            <li >
+              {{ detail.duration }}
+            </li>
+          </ul>
+        </div>
+
       </div>
     </el-card>
   </div>
@@ -64,9 +122,9 @@ export default {
   .el-card__body{
     display: flex;
   }
-  .title{
-    font-size: 16px;
-    color: #555555;
+  .movie-title{
+    font-size: 18px;
+    color: #333;
   }
   .poster{
     width: 400px;
@@ -76,7 +134,23 @@ export default {
     width: 100%;
     display: block;
   }
-  .actors li + li{
-    margin-top: 10px;
+  .item{
+    display: flex;
+    margin-top: 15px;
+  }
+  .item .item-label{
+    width: 120px;
+    color: #666666;
+    text-align: right;
+  }
+  .item .item-content{
+    flex:1;
+    display: flex;
+    flex-wrap: wrap;
+    color: #111;
+  }
+  .item .item-content li{
+    margin-bottom: 10px;
+    margin-right: 15px;
   }
 </style>
