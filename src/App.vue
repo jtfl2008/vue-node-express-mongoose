8a0868e929aa053baad2292990dab96d2639211f
jtfl2008 <jtfl2008@gmail.com>
2017-10-19 14:31:34 +0800

    结构调整

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <router-view></router-view>
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
