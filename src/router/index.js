acbe645171afc1ad913d491a383bf2cdd4bd2718
jtfl2008 <jtfl2008@gmail.com>
2017-11-06 15:43:51 +0800

    路由调整

--- src/router/index.js
+++ src/router/index.js
@@ -2,27 +2,42 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 let Home = resolve => require(['@/pages/Home'], resolve)
+let Movies = resolve => require(['@/pages/Movies'], resolve)
+let Books = resolve => require(['@/pages/Books'], resolve)
+let Musics = resolve => require(['@/pages/Musics'], resolve)
 let Detail = resolve => require(['@/pages/Detail'], resolve)
 let Login = resolve => require(['@/pages/Login'], resolve)
 let Registe = resolve => require(['@/pages/Registe'], resolve)
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home,
+    redirect: { name: 'Movies' },
+    children: [{
+      path: 'movies',
+      name: 'Movies',
+      component: Movies
     }, {
-      path: '/detail/:id',
+      path: 'books',
+      name: 'Books',
+      component: Books
+    }, {
+      path: 'musics',
+      name: 'Musics',
+      component: Musics
+    }, {
+      path: 'detail/:id',
       name: 'Detail',
       component: Detail
-    }, {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    }, {
-      path: '/registe',
-      name: 'Registe',
-      component: Registe
-    }
-  ]
+    }]
+  }, {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }, {
+    path: '/registe',
+    name: 'Registe',
+    component: Registe
+  }]
 })
